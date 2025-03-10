4b6b0f68f4a481853ade5dc496484dd11919dade
wesleytienga <wesleycoelhotienga@yahoo.com.br>
2025-03-10 22:19:47 +0000

    Nvo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
+// Desafio Super Trunfo - 
 // Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+
+
+
 
 int main() {
       printf("Desafio de super trunfo\n");
